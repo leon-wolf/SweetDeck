2f0f3f9cea665571b2e68374a3050c1ddf28964d
Leon Wolf <leon.wolf@jakala.com>
2026-02-04 21:32:48 +0100

    update

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
-        .package(url: "https://github.com/tuist/XcodeProj.git", from: "9.7.2"),
+        .package(url: "https://github.com/tuist/XcodeProj.git", branch: "main"),
     ],
     targets: [
         .executableTarget(
@@ -54,4 +54,3 @@ let package = Package(
         ),
     ]
 )
-
