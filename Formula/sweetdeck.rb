00d6a1c5e6fa9cb79aefe01a7e7569cc0c0dd5cf
Leon Wolf <leon.wolf@jakala.com>
2026-02-04 22:09:16 +0100

    update release flow

--- Formula/sweetdeck.rb
+++ Formula/sweetdeck.rb
@@ -1,9 +1,9 @@
 class Sweetdeck < Formula
   desc "FlowDeck-like CLI for Xcode builds and simulator workflows"
   homepage "https://github.com/leon-wolf/SweetDeck"
-  url "https://github.com/leon-wolf/SweetDeck/releases/download/v0.1.0/sweetdeck-v0.1.0-macos.zip"
+  url "https://github.com/leon-wolf/SweetDeck/releases/download/v__VERSION__/sweetdeck-v__VERSION__-macos.zip"
   sha256 "REPLACE_SHA256"
-  version "0.1.0"
+  version "__VERSION_NO_V__"
 
   def install
     bin.install "sweetdeck"
@@ -13,4 +13,3 @@ class Sweetdeck < Formula
     system "#{bin}/sweetdeck", "--version"
   end
 end
-
